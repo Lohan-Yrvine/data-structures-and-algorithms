6998fb4fed4a9288bd09ad15b3c3d3f46c40ef31
Lohan Pinheiro <o.lohan.yrvine@gmail.com>
2022-10-15 10:52:39 -0300

    hash table implementation

---
+++ data-structures/hash-table/implementation/hash_table.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hash_table.h"
+
+#define TABLE_SIZE 128
+
+struct hash_table* new_hash_table(void)
+{
+    struct hash_table *table;
+    table->arr = (struct node**) malloc(TABLE_SIZE * sizeof(struct node));
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        table->arr[i] = NULL;
+    }
+
+    return table;
+}
+
+int hash_function(int key)
+{
+    return key % TABLE_SIZE;
+}
+
+void insert_element(struct hash_table *table, int key, int value)
+{
+    int h = hash_function(key);
+
+    struct node* n = (struct node*) malloc(sizeof(struct node));
+    n->key = h;
+    n->value = value;
+
+    table->arr[h] = n;
+}
+
+int get_value(struct hash_table *table, int key)
+{
+    int h = hash_function(key);
+    return table->arr[h]->value;
+}
+
+void remove_element(struct hash_table *table, int key)
+{
+    int h = hash_function(key);
+    free(table->arr[h]);
+    table->arr[h] = NULL;
+}
+
+void print(struct hash_table *table)
+{
+    printf("{\n");
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table->arr[i] != NULL)
+        {
+            printf("\t%d: %d\n", table->arr[i]->key, table->arr[i]->value);
+        }
+    }
+    printf("}\n");
+}
---
+++ data-structures/hash-table/implementation/hash_table.h
@@ -0,0 +1,25 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+struct node
+{
+    int key;
+    int value;
+};
+
+struct hash_table
+{
+    struct node **arr;
+};
+
+struct hash_table* new_hash_table(void);
+
+int hash_function(int key);
+void insert_element(struct hash_table *table, int key, int value);
+
+int get_value(struct hash_table *table, int key);
+void remove_element(struct hash_table *table, int key);
+
+void print(struct hash_table *table);
+
+#endif
