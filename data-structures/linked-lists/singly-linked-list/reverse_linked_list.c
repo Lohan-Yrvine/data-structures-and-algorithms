f0365a3e142a228dc7faee05d98ec23e7b20385f
Lohan Pinheiro <o.lohan.yrvine@gmail.com>
2022-10-21 17:09:32 -0300

    reversing linked list

---
+++ data-structures/linked-lists/singly-linked-list/reverse_linked_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include "implementation/singly_linked_list.h"
+
+void reverse_linked_list(struct singly_linked_list *ll)
+{
+    struct node *prev = NULL;
+    struct node *actual = ll->head;
+    struct node *next = ll->head;
+
+    while (next != NULL)
+    {
+        next = next->next;
+        actual->next = prev;
+        prev = actual;
+        actual = next;
+    }
+
+    ll->head = prev;
+}
+
+int main(void)
+{
+    struct singly_linked_list *ll = new_singly_linked_list();
+
+    for (int i = 0; i <= 5; i++)
+    {
+        insert_element(ll, i, i);
+    }
+
+    display(ll);
+
+    reverse_linked_list(ll);
+    display(ll);
+
+    return 0;
+}
